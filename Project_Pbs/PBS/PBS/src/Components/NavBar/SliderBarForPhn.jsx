4872c596647f20c59459f6ff951a6fae213e8377
Sagar-masale <sagarmasale27@gmail.com>
2024-08-26 19:46:57 +0530

    Update files

--- Project_Pbs/PBS/PBS/src/Components/NavBar/SliderBarForPhn.jsx
+++ Project_Pbs/PBS/PBS/src/Components/NavBar/SliderBarForPhn.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 import './SliderBarForPhn.css';
 function SliderBarForPhn() {
 	const SlideBarValue =(e)=>{
@@ -14,8 +15,8 @@ function SliderBarForPhn() {
 	  const [userSlideSearchValue, setUserSlideSearchValue]=useState('');	
   return (
     <>
-<div className="flex flex-col h-full  dark:bg-gray-50 dark:text-gray-800">
-	<div className="space-y-3">
+<div className="flex flex-col h-full  dark:bg-red-40 dark:text-gray-800">
+	<div className="space-y-3 ">
 	 <div className="SliderBar-Color-Box w-full ">
 	 <div className="flex items-center justify-between">
         <h2 className='text-red-900'>PBS</h2>
@@ -40,71 +41,83 @@ function SliderBarForPhn() {
 	 </div>
 		<div className="flex-1 pl-2">
 			<ul className="pt-2   space-y-1 text-sm flex flex-col gap-3">
-				<li className="rounded-sm ">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
-					<span class="material-symbols-outlined text-red-900">
-                     home
-                    </span>
+				<Link to='/'>
+				<li className="rounded-sm " onClick={SliderMove}>
+					<span className='flex items-center p-2 space-x-3 rounded-md'>
+						
+						<button class="material-symbols-outlined text-red-900">
+                         home
+                        </button>
+						
 						<span>Home</span>
-					</a>
+					</span>
 				</li>
+				</Link>
 				<li className="rounded-sm">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      search
                     </span>
 						<span>Search</span>
-					</a>
+					</button>
 				</li>
 				<li className="rounded-sm">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      chat
                     </span>
 						<span>Chat</span>
-					</a>
+					</button>
 				</li>
 				<li className="rounded-sm">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      orders
                     </span>
 						<span>Orders</span>
-					</a>
+					</button>
 				</li>
 				<li className="rounded-sm  dark:text-gray-900">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      favorite
                     </span>
 						<span>Wishlist</span>
-					</a>
+					</button>
 				</li>
 				<li className="rounded-sm">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      settings
                     </span>
 						<span>Settings</span>
-					</a>
+					</button>
 				</li>
-				<li className="rounded-sm">
-					<a rel="noopener noreferrer" href="#" className="flex items-center p-2 space-x-3 rounded-md">
+				<li className="rounded-sm User-LogIn">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
+					<span class="material-symbols-outlined  text-red-900">
+                     login
+                    </span>
+						<span>Log In</span>
+					</button>
+				</li>
+				<li className="rounded-sm User-LogOunt">
+					<button className="flex items-center p-2 space-x-3 rounded-md">
 					<span class="material-symbols-outlined text-red-900">
                      logout
                     </span>
 						<span>Logout</span>
-					</a>
+					</button>
 				</li>
 			</ul>
 		</div>
 	</div>
-	<div className="flex items-center pl-2  mt-5 space-x-4 justify-self-end">
+	<div className="flex items-center pl-2  mt-5 space-x-4 justify-self-end Profile-Box">
 		<img src="https://www.pngitem.com/pimgs/m/130-1300253_female-user-icon-png-download-user-image-color.png" alt="" className="w-12 h-12 rounded-lg dark:bg-gray-500" />
-		<div>
+		<div> 
 			<h2 className="text-lg font-semibold">Hi Oggey </h2>
 			<span className="flex items-center space-x-1">
-				<a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-600">View profile</a>
+				<button className="text-xs hover:underline dark:text-gray-600">View profile</button>
 			</span>
 		</div>
 	</div>
